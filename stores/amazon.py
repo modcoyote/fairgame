cfa4cb4128e01e23a26cd25f1892c1df0ed2e2f7
FRACTAL\Krzysztof <modcoyote@gmail.com>
2021-03-02 17:59:28 +0100

    logging price when item found

--- stores/amazon.py
+++ stores/amazon.py
@@ -793,6 +793,8 @@ class Amazon:
                 (ship_float + price_float) >= reserve_min
                 or math.isclose((price_float + ship_float), reserve_min, abs_tol=0.01)
             ):
+                log.info("Item found at price " + price_float)
+                log.info("Item shipping cost " + ship_float)
                 log.info("Item in stock and in reserve range!")
                 log.info("Adding to cart")
                 # Get the offering ID
